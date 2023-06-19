d46281a5abadf6b40831ebb7d5cf36521c6d912a
Jeremy Corbeil <jeremycorbeil9@yahoo.com>
2023-06-19 12:06:51 -0400

    phase-1-sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,96 @@
-// Add your code here
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, config);
+  }
+  function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, config)
+      .then(response => response.json())
+      .then(parsedResponse => {
+        const id = parsedResponse.id;
+        document.body.append(id);
+      });
+  }
+  function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, config)
+      .then(response => response.json())
+      .then(parsedResponse => {
+        const id = parsedResponse.id;
+        document.body.append(id);
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        document.body.append(errorMessage);
+      });
+  }
+  function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, config)
+      .then(response => response.json())
+      .then(parsedResponse => {
+        const id = parsedResponse.id;
+        document.body.append(id);
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        document.body.append(errorMessage);
+      });
+  }
+  
